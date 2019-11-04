c713bdfe77596a86f17ce0584c83e91bea0dc6c4
Dom December <21.domantas.gruodis@azuolynogimnazija.lt>
2019-11-04 15:30:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -51,5 +51,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect{|element, index| index != 2 ? element + "s" }
+  array.each_with_index.collect{|element, index| if index != 2 {element + "s"} }
 end
